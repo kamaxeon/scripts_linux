9ebf6e04b0ccf7e58b618c4d9d9a36895655a4c0
Israel Santana <isra@miscorreos.org>
2014-03-13 22:34:45 +0000

    Cambiando el mensaje de headers

--- elastix/drbd/header.sh
+++ elastix/drbd/header.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-echo "Script para montar dos maquinas Elastix"
-echo "en cluster usando drbd"
+echo "Script para montar dos maquinas Elastix en cluster usando DRBD"
 echo "Basados en http://www.theserverexpert.com/elastix_2.4_ha_cluster-updated.pdf"
 
 if [ "$(id -u)" != "0" ]; then
